f75b70d9ea1ffc4f284e11dc79a2bb3eb09e9b78
unknown <adityasingh2810@gmail.com>
2021-05-12 21:06:28 +0530

    Committing the newly added files

---
+++ src/components/ClassClick.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react'
+
+ class ClassClick extends Component {
+
+    clickEvent(){
+        console.log('Clicked the button')
+    }
+
+    render() {
+        return (
+            <div>
+                <button onClick={this.clickEvent}>Click Me</button>
+            </div>
+        )
+    }
+}
+
+export default ClassClick
